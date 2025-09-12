38c8c0072a12207ec9e89579b9eaa2a2cf9d8653
Favour <gh.oghene@gmail.com>
2025-09-12 22:40:19 +0100

    added weather manager struct

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -15,6 +15,8 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var searchTextField: UITextField!
     
+    var weatherManager = WeatherManager()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,6 +43,11 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if let cityName = searchTextField.text {
+            weatherManager.fetchWeather(cityName)
+        }
+        
         searchTextField.text = ""
     }
     
