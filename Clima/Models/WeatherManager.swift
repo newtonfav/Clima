0b9f836626f2527d4af300c6ac62be7c79765c04
Favour <gh.oghene@gmail.com>
2025-09-19 10:24:36 +0100

    match weather id

--- Clima/Models/WeatherManager.swift
+++ Clima/Models/WeatherManager.swift
@@ -3,7 +3,8 @@ import Foundation
 struct WeatherManager {
     let weatherUrl =
         "https://api.openweathermap.org/data/3.0/onecall?exclude=minutely,hourly,daily&appid=0ad817eb245fe651cab840fabf7056f1&units=metric"
-    let geocodingUrl = "https://api.openweathermap.org/geo/1.0/direct?&appid=0ad817eb245fe651cab840fabf7056f1&limit=1"
+    let geocodingUrl =
+        "https://api.openweathermap.org/geo/1.0/direct?&appid=0ad817eb245fe651cab840fabf7056f1&limit=1"
 
     func fetchCityCordinates(_ cityName: String) {
         let urlString = "\(geocodingUrl)&q=\(cityName)"
@@ -23,7 +24,7 @@ struct WeatherManager {
                     let coordinates = parseGeocodingJSON(
                         geocodingData: safeData
                     )
-                    
+
                     fetchWeather(
                         longitude: coordinates?.lat ?? 0.0,
                         latitude: coordinates?.lon ?? 0.0
@@ -85,8 +86,34 @@ struct WeatherManager {
                 from: weatherData
             )
             print(decodedData.current.weather[0].description)
+            let weatherId = decodedData.current.weather[0].id
+            let conditionName = getWeatherConditionName(weatherId)
+            print(weatherId)
+            print(conditionName)
         } catch {
             print(error)
         }
+    }
+
+    func getWeatherConditionName(_ weatherId: Int) -> String {
+        switch weatherId {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud.bolt"
+        default:
+            return "cloud"
+        }
+
     }
 }
